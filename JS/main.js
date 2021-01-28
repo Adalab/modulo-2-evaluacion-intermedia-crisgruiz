ed8ca870386ab9b17479307f857d811f24674191
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-01-28 09:18:44 +0100

    Add html and css files

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+"user strict";
+
+//Función para generar un número aleatorio
+function getRandomNumber(max) {
+    return Math.ceil(Math.random()*100);
+}
+const numberRandom = getRandomNumber();
+console.log(numberRandom);
+
+//Función para comprobar si es el número correcto.
+
+const NumberElement =
+
+function handleNumber(){
+
+}
