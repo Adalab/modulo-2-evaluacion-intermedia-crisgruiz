f78a63b9e6bff94c24668a12dce9056b63bf83ac
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-01-28 09:44:56 +0100

    Add random number function and condition function

--- JS/main.js
+++ JS/main.js
@@ -9,8 +9,31 @@ console.log(numberRandom);
 
 //Función para comprobar si es el número correcto.
 
-const NumberElement =
+const NumberElement = document.querySelector('.js-number')
+const btnTryElement = document.querySelector('.js-tryBtn')
+const TextElement = document.querySelector('.js-message')
+
 
 function handleNumber(){
-
+    const numberValue = parseInt(NumberElement.value);
+    if (numberValue === numberRandom){
+        TextElement.innerHTML = '¡Enhorabuena! ¡¡¡Has ganado campeona!!!'
+    }
+    else if (numberValue>100){
+        TextElement.innerHTML = 'Tienes que introducir un número entre 1 y 100'
+    }
+    else if (numberValue<1){
+        TextElement.innerHTML = 'Tienes que introducir un número entre 1 y 100'
+    }
+    else if (numberValue < numberRandom){
+    TextElement.innerHTML = 'El número que buscamos es más alto. Vuelve a intentarlo'
 }
+    else if (numberValue > numberRandom){
+    TextElement.innerHTML = 'El número que buscamos es más bajo. Vuelve a intentarlo'
+}
+else{
+    TextElement.innerHTML = 'Tienes que introducir un número'
+}
+};
+
+btnTryElement.addEventListener('click', handleNumber);
